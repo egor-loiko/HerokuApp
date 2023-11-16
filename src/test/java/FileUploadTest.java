65bdeb38962f62b3acb13ebbc7db94f5e0dd167e
egor.loiko <egor.loiko@softswiss.com>
2023-11-16 09:05:46 +0300

    TMS-2 Added File Upload test

---
+++ src/test/java/FileUploadTest.java
@@ -0,0 +1,44 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
+public class FileUploadTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void dynamicControls() {
+        Actions actions = new Actions(driver);
+        String fileName = "bitrix producer - send message to kafka.txt";
+        File file = new File("src\\test\\resources\\" + fileName);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        driver.findElement(By.id("file-upload")).sendKeys(file.getAbsolutePath());
+        driver.findElement(By.id("file-submit")).click();
+        assertEquals(driver.findElement(By.xpath("//div[@id='content']//h3")).getText(), "File Uploaded!", "File is not uploaded");
+        assertEquals(driver.findElement(By.id("uploaded-files")).getText(), fileName, "File with invalid name is uploaded");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
